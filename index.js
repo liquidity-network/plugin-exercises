04b7666d3e5810b50c1f5098800540bd59cd4b51
Thibault Meunier <wognovice@gmail.com>
2018-04-18 13:43:16 +0100

    Location of the private key is now local storage
    Refactor transformTest function
     * Split it into different ones
    
    Add comments to all functions
     * In JsDoc format

--- index.js
+++ index.js
@@ -1,80 +1,154 @@
-var _ = require('lodash');
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
-var solc = require('solc');
-var linker = require('solc/linker');
-var Web3 = require('web3');
+let _ = require('lodash');
+let fs = require('fs');
+let path = require('path');
+let solc = require('solc');
+let linker = require('solc/linker');
+let Web3 = require('web3');
 
-var web3 = new Web3('https://kovan.infura.io');
+let web3 = new Web3('https://kovan.infura.io');
 
-var WEBSITE_TPL = _.template(fs.readFileSync(path.resolve(__dirname, "./assets/website.html")));
-var EBOOK_TPL = _.template(fs.readFileSync(path.resolve(__dirname, "./assets/ebook.html")));
+let WEBSITE_TPL = _.template(fs.readFileSync(path.resolve(__dirname, './assets/website.html')));
+let EBOOK_TPL = _.template(fs.readFileSync(path.resolve(__dirname, './assets/ebook.html')));
 
+/**
+ * Retrieve the private key from a remote website
+ * @dev It should be changed to a local retrieve
+ * @returns {string} - Private key used to deploy tests
+ */
 function getPrivateKey() {
-    var url = 'https://www.thibaultmeunier.com/private.key';
-
-    return new Promise(function (resolve, reject) {
-        request.get({
-            url: url,
-            json: true,
-            headers: {'User-Agent': 'request'}
-        }, function(err, res, data) {
-            if(err) {
-                reject();
-            } else {
-                resolve('0x' + data);
-            }
-        });
-    });
+    let file = fs.readFileSync(path.resolve(__dirname, '../../private.key'), 'utf8')
+    return '0x' + JSON.parse(file)
 }
 
-// Should go in its own npm package
-function parseSolidityJSON(name, interfaceStr) {
-    var interface = JSON.parse(interfaceStr);
-
-    var interfaceTxt = 'pragma solidity ^0.4.21;\ninterface ' + name + ' {\n';
+/**
+ * From a standard solidity compiler JSON output, create the corresponding solidity interface in Solidity
+ * @dev Should go in its own npm package
+ * @param {string} name - name of the interface
+ * @param {Object} interfaceJSON - interface described with standard solidify compiler JSON output
+ * @returns {string} - interface written in solidity
+ */
+function parseSolidityJSON(name, interfaceJSON) {
+    let interfaceTxt = 'pragma solidity ^0.4.21;\ninterface ' + name + ' {\n';
 
     interfaceTxt +=
-        interface.filter(function (obj) {
+        interfaceJSON.filter(function (obj) {
             return obj.type === 'function'
         }).map(function (obj) {
-            var result = '    ';
-            result += obj.type;
-            result += ' ' + obj.name;
-            result += '(' + obj.inputs.map(function (input) {
-                return input.type + ' ' + input.name
-            }).join(', ') + ')';
-            result += ' ' + 'external';
-            result += ' ' + obj.stateMutability;
-            if (obj.payable) {
-                result += ' ' + 'payable';
-            }
-            if (obj.outputs) {
-                result += ' returns (' + obj.outputs.map(function (output) {
-                    return output.type
-                }).join(', ') + ')';
-            }
-            return result + ';';
+            return [
+                obj.type,
+                obj.name,
+                '(' + obj.inputs.map(function (input) {
+                    return input.type + ' ' + input.name
+                }).join(', ') + ')',
+                'external',
+                obj.stateMutability,
+                (obj.payable ? 'payable' : ''),
+                (obj.outputs ?
+                    ' returns (' + obj.outputs.map(function (output) {
+                        return output.type
+                    }).join(', ') + ')' : ''),
+                ';'
+            ].join(' ')
         }).join('\n');
 
     interfaceTxt += '\n}';
     return interfaceTxt;
 }
 
-// @param test test file as a string
-// @param contracts array containing names of the contracts
-// @return result transformed test file for compilation
-function transformSolidityTest(test, contracts) {
-    var result = '';
+/**
+ * From a public function returns its new version, where contracts are bind from user input
+ * @dev function name ( ...args, address[] _addresses ) public { ...(Var let = _addresses[i];) _name(...args, ...(vars)); }
+ * @param {string} name - name of the function
+ * @param {Array<{type: string, name: string}>} args - arguments of the function
+ * @param {Array<{type: string, name: string}>} variables - variables of the contracts
+ * @returns {string} - new public function
+ */
+function buildPublicFunctionForTransform(name, args, variables) {
+    return [
+        'function',
+        name,
+        '(',
+        (args > 0 ? args.map(function (v) {
+            return v.type + ' ' + v.name
+        }).join(',') : ''),
+        (args > 0 ? ', ' : ''),
+        'address[] _addresses',
+        ')',
+        'public',
+        '{',
+        variables.map(function (v, index) {
+            return [v.type, v.name, '=', v.type, '(_addresses[', index, '])'].join(' ')
+        }).join(';') + ';',
+        '_' + name,
+        '(',
+        variables.map(function (v) {
+            return v.name
+        }).join(', '),
+        ')',
+        ';',
+        '}'
+    ].join(' ')
+}
 
-    var variables = [];
+/**
+ * From a public function, makes it private with name `_name` and create a new public function to bind user contracts as arguments
+ * @param {string} line - Line containing the funciton signature
+ * @param {Array<{type: string, name: string}>} variables - variables of the contracts
+ * @returns {string} - public and private generated functions
+ */
+function transformFunction(line, variables) {
+    let signature = line.replace(new RegExp(', ', 'g'), ',').split(' ')[1];
+    let name = signature.split('(')[0];
+    let args = signature.split('(')[1].slice(0, -1); //arguments of the function, slice to remove ending ')'
+    if (args) {
+        args = args.split(',').map(function (arg) {
+            arg = arg.trim().split(' ');
+            return {
+                type: arg[0],
+                name: arg[1]
+            }
+        });
+    } else {
+        args = [];
+    }
+
+    let parameters = args.concat(variables);
+
+    // Create new public function
+    let result = buildPublicFunctionForTransform(name, args, variables);
+
+    // Old function to private one
+    result += [
+        'function',
+        '_' + name,
+        '(',
+        parameters.map(function (v) {
+            return v.type + ' ' + v.name
+        }).join(', '),
+        ')',
+        'private',
+        '{'
+    ].join(' ');
+    
+    return result
+}
+
+/**
+ * From a standard truffle test makes abstract the contract to make it testable with different versions
+ * @param test - test file as a string
+ * @param {Array<string>} contracts - array containing names of the contracts
+ * @returns {string} - transformed test file for compilation
+ */
+function transformSolidityTest(test, contracts) {
+    let result = '';
+
+    let variables = [];
     test = test.split('\n').map(function (line) {
         return line.trim()
     });
-    for (var index = 0; index < test.length; index++) {
-        var line = test[index];
-        var contractType = _.reduce(
+    for (let index = 0; index < test.length; index++) {
+        let line = test[index];
+        let contractType = _.reduce(
             contracts,
             function (acc, name) {
                 return ((!acc && line.startsWith(name)) ? name : acc)
@@ -82,68 +156,13 @@ function transformSolidityTest(test, contracts) {
             ''
         );
         if (contractType) {
-            var name = line.split(' ')[1];
+            let name = line.split(' ')[1];
             variables.push({
                 type: contractType,
                 name: (name.endsWith(';') ? name.slice(0, -1) : name)
             });
         } else if (line.startsWith('function')) {
-            var signature = line.replace(new RegExp(', ', 'g'), ',').split(' ')[1];
-            var name = signature.split('(')[0];
-            var args = signature.split('(')[1].slice(0, -1); //arguments of the function, slice to remove ending ')'
-            if (args) {
-                args = args.split(',').map(function (arg) {
-                    arg = arg.trim().split(' ');
-                    return {
-                        type: arg[0],
-                        name: arg[1]
-                    }
-                });
-            } else {
-                args = [];
-            }
-
-            var parameters = args.concat(variables);
-
-            // @dev function name ( ...args, address[] _addresses ) public { ...(Var var = _addresses[i];) _name(...args, ...(vars)); }
-            result += [
-                'function',
-                name,
-                '(',
-                (args > 0 ? args.map(function (v) {
-                    return v.type + ' ' + v.name
-                }).join(',') : ''),
-                (args > 0 ? ', ' : ''),
-                'address[] _addresses',
-                ')',
-                'public',
-                '{',
-                variables.map(function (v, index) {
-                    return [v.type, v.name, '=', v.type, '(_addresses[', index, '])'].join(' ')
-                }).join(';') + ';',
-                '_' + name,
-                '(',
-                variables.map(function (v) {
-                    return v.name
-                }).join(', '),
-                ')',
-                ';',
-                '}'
-            ].join(' ');
-
-            // old function to private one
-            // @dev function _name ( ...args ) private {
-            result += [
-                'function',
-                '_' + name,
-                '(',
-                parameters.map(function (v) {
-                    return v.type + ' ' + v.name
-                }).join(', '),
-                ')',
-                'private',
-                '{'
-            ].join(' ');
+            result += transformFunction(line, variables);
         } else {
             result += line + '\n';
         }
@@ -152,11 +171,16 @@ function transformSolidityTest(test, contracts) {
     return result;
 }
 
+/**
+ * Deploy a given contract using the account registered in web3
+ * @param {{interface: string, bytecode: string}} contract - contract to deploy
+ * @returns {Promise<string>} - address of the deployed contract
+ */
 function deploy(contract) {
-    var abi = contract.interface;
-    var bc = '0x' + contract.bytecode; // web3 expect bytecode to be written in hexadecimal
+    let abi = contract.interface;
+    let bc = '0x' + contract.bytecode; // web3 expect bytecode to be written in hexadecimal
 
-    var mContract = new web3.eth.Contract(JSON.parse(abi));
+    let mContract = new web3.eth.Contract(JSON.parse(abi));
 
     return new Promise(function (resolve) {
         mContract.deploy({
@@ -178,24 +202,38 @@ function deploy(contract) {
     });
 }
 
+/**
+ * Given the compiled version of contracts, create their associated solidity interface
+ * @dev Should this function return an object to keep the interface associated with its name?
+ * @param {{contracts: Array<{interace: string}>}} codes - Solidity compiler output
+ * @returns {Array<{name: string, code: string}>} - All tests interfaces
+ */
 function createInterfaces(codes) {
     return Object.keys(codes.contracts).map(function (fullname) {
-        var name = (new RegExp('([^:]+)$')).exec(fullname)[0].trim();
+        let name = (new RegExp('([^:]+)$')).exec(fullname)[0].trim();
+        let interfaceStr = codes.contracts[fullname].interface
         return {
             name: name,
-            code: parseSolidityJSON(name, codes.contracts[fullname].interface)
+            code: parseSolidityJSON(name, JSON.parse(interfaceStr))
         };
     });
 }
 
+/**
+ * First deploy the Assert library to the blockchain, then deploy all tests to the blockchain
+ * @dev it could be optimized by deploying the Assert library
+ * @param {{contracts: Array<{interace: string}>}} codes - Solidity compiler output
+ * @param {Array<string>} toDeploy - Name of contracts to be deployed
+ * @returns {Array<{address: string, abi: string}>} - All tests information: abi and address
+ */
 async function deployTests(codes, toDeploy) {
-    var tests = [];
+    let tests = [];
 
     // First deploy assert library
-    var assertAddress = await deploy(codes.contracts['Assert.sol:Assert']);
+    let assertAddress = await deploy(codes.contracts['Assert.sol:Assert']);
 
-    for (var index = 0; index < toDeploy.length; index++) {
-        var key = toDeploy[index];
+    for (let index = 0; index < toDeploy.length; index++) {
+        let key = toDeploy[index];
         // Link test with the already deployed assert library
         codes.contracts[key].bytecode =
             linker.linkBytecode(
@@ -203,7 +241,7 @@ async function deployTests(codes, toDeploy) {
                 {'Assert.sol:Assert': assertAddress}
             );
         // Deploy the test
-        var address = await deploy(codes.contracts[key]);
+        let address = await deploy(codes.contracts[key]);
         tests.push({
             address: address,
             abi: codes.contracts[key].interface
@@ -213,13 +251,19 @@ async function deployTests(codes, toDeploy) {
     return tests;
 }
 
-async function compileAndDeploy(codes, book) {
+/**
+ * Compile and deploy all test of the exercise
+ * @param {{solution: string, validation: string}} codes - Raw solidity code from the exercise, @param{solution} is the solution provided by the tester and will help to build a generic interface of all solution, @param{validation} is the code for all the tests
+ * @param {string} assertLibrary - Solidity file of the Assert library
+ * @returns {Array<{address: string, abi: string}>} - All tests information: abi and address
+ */
+async function compileAndDeploy(codes, assertLibrary) {
     // Compile the solution
-    var cSolution = solc.compile({sources: {'solution.sol': codes.solution}}, 1);
+    let cSolution = solc.compile({sources: {'solution.sol': codes.solution}}, 1);
 
     // Create an interface for every contract the user will code
-    var interfaces = createInterfaces(cSolution);
-    var names = interfaces.map(function (snip) {
+    let interfaces = createInterfaces(cSolution);
+    let names = interfaces.map(function (snip) {
         return snip.name
     });
 
@@ -227,15 +271,15 @@ async function compileAndDeploy(codes, book) {
     codes.validation = transformSolidityTest(codes.validation, names);
 
     // Compile interfaces, assert library and test code
-    var input = _.reduce(interfaces, function (acc, interface) {
-        var m = {};
+    let input = _.reduce(interfaces, function (acc, interface) {
+        let m = {};
         m[interface.name + '.sol'] = interface.code;
         return _.extend(acc, m);
     }, {});
-    input['Assert.sol'] = fs.readFileSync(book.resolve('sol/Assert.sol'), 'utf8');
+    input['Assert.sol'] = assertLibrary
     input['test.sol'] = codes.validation;
 
-    var cTests = solc.compile({sources: input}, 1);
+    let cTests = solc.compile({sources: input}, 1);
 
     if (cTests.errors) {
         throw new Error('Compilation failed\n' + cTests.errors.join('\n'))
@@ -244,33 +288,39 @@ async function compileAndDeploy(codes, book) {
     // Deployment
 
     // Remaining contracts to deploy (i.e. tests)
-    var toDeploy = Object.keys(cTests.contracts)
+    let toDeploy = Object.keys(cTests.contracts)
         .filter(function (key) {
             return key.startsWith('test.sol')
         });
 
     // It should be possible to deploy contracts asynchronously
-    var tests = await deployTests(cTests, toDeploy);
+    let tests = await deployTests(cTests, toDeploy);
 
     return tests;
 }
 
+/**
+ * Manage all pre-operations necessary for the exercise to work
+ * @param {{blocks: Array<{name: string, body: string}>}} blk - Information about the block being parsed
+ * @returns {string} - HTML code to insert into the webpage
+ */
 async function process(blk) {
-    var PRIVATE_KEY = await getPrivateKey();
+    let PRIVATE_KEY = getPrivateKey();
     web3.eth.accounts.wallet.add(PRIVATE_KEY);
 
-    var codes = {};
+    let codes = {};
 
     _.each(blk.blocks, function (_blk) {
         codes[_blk.name] = _blk.body.trim();
     });
 
     // Compile and deploy test contracts to our blockchain
-    var tests = await compileAndDeploy(codes, this.book);
+    let assertLibrary = fs.readFileSync(this.book.resolve('sol/Assert.sol'), 'utf8');
+    let tests = await compileAndDeploy(codes, assertLibrary);
     codes.deployed = JSON.stringify(tests);
 
     // Select appropriate template
-    var tpl = (this.generator === 'website' ? WEBSITE_TPL : EBOOK_TPL);
+    let tpl = (this.generator === 'website' ? WEBSITE_TPL : EBOOK_TPL);
 
     return tpl({
         message: blk.body,
